c860bd740133d05fd2e2f6c3e42a71bfaa574379
mactavishz <maczhanchao@yahoo.com>
2025-05-31 15:54:43 +0200

    feat: add String() to ast node interface for better print debugability

--- lib/parse/ast.go
+++ lib/parse/ast.go
@@ -1,6 +1,9 @@
 package parse
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // ModifierType enum
 type ModifierType int
@@ -15,6 +18,7 @@ const (
 type ASTNode interface {
 	ToRegex() string
 	NodeType() string
+	String() string
 }
 
 // Modifier node - represents repetition modifiers
@@ -46,6 +50,10 @@ func (q *Modifier) NodeType() string {
 	return "Quantifier"
 }
 
+func (q *Modifier) String() string {
+	return fmt.Sprintf("Modifier {\n  Child: %s,\n  Type: %d\n}", indentString(q.Child.String(), 2), q.Type)
+}
+
 // Top-level AstNode
 type Expression struct {
 	Child ASTNode
@@ -57,6 +65,10 @@ func (e *Expression) ToRegex() string {
 
 func (e *Expression) NodeType() string {
 	return "Expression"
+}
+
+func (e *Expression) String() string {
+	return fmt.Sprintf("Expression {\n  Child: %s\n}", indentString(e.Child.String(), 2))
 }
 
 type Term struct {
@@ -74,6 +86,14 @@ func (t *Term) NodeType() string {
 	} else {
 		return "Term"
 	}
+}
+
+func (t *Term) String() string {
+	modifier := "ModifierNone"
+	if t.HasModifier {
+		modifier = "Modified"
+	}
+	return fmt.Sprintf("Term {\n  Child: %s,\n  Modifier: %s\n}", indentString(t.Child.String(), 2), modifier)
 }
 
 // Alternation node - represents OR operation (a|b)
@@ -96,6 +116,10 @@ func (cat *Concatenation) NodeType() string {
 	return "Concatenation"
 }
 
+func (cat *Concatenation) String() string {
+	return fmt.Sprintf("Concatenation {\n  Left: %s,\n  Right: %s\n}", indentString(cat.Left.String(), 2), indentString(cat.Right.String(), 2))
+}
+
 // Character node - represents a single character
 type Character struct {
 	Value rune
@@ -107,6 +131,10 @@ func (c *Character) ToRegex() string {
 
 func (c *Character) NodeType() string {
 	return "Character"
+}
+
+func (ch *Character) String() string {
+	return fmt.Sprintf("Character {Value: '%c'}", ch.Value)
 }
 
 // Group node - represents parenthesized expression
@@ -122,10 +150,36 @@ func (g *Group) NodeType() string {
 	return "Group"
 }
 
+func (g *Group) String() string {
+	return fmt.Sprintf("Group {\n  Expr: %s\n}", indentString(g.Expr.String(), 2))
+}
+
 func (a *Alternation) ToRegex() string {
 	return fmt.Sprintf("%s|%s", a.Left.ToRegex(), a.Right.ToRegex())
 }
 
 func (a *Alternation) NodeType() string {
 	return "Alternation"
+}
+
+func (a *Alternation) String() string {
+	return fmt.Sprintf("Alternation {\n  Left: %s,\n  Right: %s\n}", indentString(a.Left.String(), 2), indentString(a.Right.String(), 2))
+}
+
+// indentString indents each line of s by n spaces
+func indentString(s string, n int) string {
+	if n <= 0 {
+		return s
+	}
+
+	prefix := strings.Repeat(" ", n)
+	lines := strings.Split(s, "\n")
+
+	for i := range lines {
+		if i > 0 { // Don't indent the first line
+			lines[i] = prefix + lines[i]
+		}
+	}
+
+	return strings.Join(lines, "\n")
 }
