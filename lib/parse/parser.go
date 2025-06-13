c397e832cc319aefa70c18c9c41593d6c3b9a9e7
mactavishz <maczhanchao@yahoo.com>
2025-06-13 16:35:08 +0200

    fix: fix grammar issue, adjust implementation, add syntax error

--- lib/parse/parser.go
+++ lib/parse/parser.go
@@ -2,13 +2,14 @@ package parse
 
 import (
 	"fmt"
+	"strings"
 )
 
 /*
 *
 * Grammar for the regular expression (in EBNF):
-*	expr := term ['|' term]*
-*   term := mterm*
+*	expr := [term ['|' expr]]
+*   term := mterm+
 *   mterm := item [modifier]
 *	modifier := '*' | '+' | '?'
 *	item := char | group
@@ -16,6 +17,19 @@ import (
 *	char -> 'a'| 'b' | 'c' | ..., UTF8 character excluding '.', '|', '*', '+', '?', '(', ')'
 *
  */
+
+type SyntaxError struct {
+	Column  int
+	Message string
+	Line    string
+}
+
+func (s SyntaxError) Error() string {
+	res := fmt.Sprintf("SyntaxError (column %d): %s\n", s.Column, s.Message)
+	res += fmt.Sprintf(">>>%s\n", s.Line)
+	res += fmt.Sprintf(">>>%s^\n", strings.Repeat("-", s.Column))
+	return res
+}
 
 type Parser struct {
 	tokenizer *Tokenizer
@@ -28,7 +42,7 @@ func NewParser(input string) *Parser {
 }
 
 func (p *Parser) match(expected TokenType) (*Token, error) {
-	t := p.tokenizer.next()
+	t := p.tokenizer.consume()
 	if t.Type != expected {
 		return t, fmt.Errorf("unexpected token, expected %s but got %s", expected.String(), t.String())
 	} else {
@@ -45,86 +59,146 @@ func (p *Parser) check(expected TokenType) bool {
 	}
 }
 
-func (p *Parser) Parse() ASTNode {
+func (p *Parser) Parse() (ASTNode, error) {
 	root := &Expression{}
-	root.Child = p.parseExpr()
-	// we only consume EOF at the top level
-	_, err := p.match(EOF)
+	child, err := p.parseExpr()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return root
+	root.Child = child
+	// we only consume EOF at the top level
+	token, err := p.match(EOF)
+	if err != nil {
+		return nil, &SyntaxError{
+			Column:  token.Column,
+			Message: "expected EOF",
+			Line:    p.tokenizer.input,
+		}
+	}
+	return root, nil
 }
 
-func (p *Parser) parseTerm() ASTNode {
+func (p *Parser) parseTerm() (ASTNode, error) {
 	if p.check(EOF) || p.check(OR) {
-		return nil
+		return nil, &SyntaxError{
+			Column:  p.tokenizer.buf.Column,
+			Message: "expected a term",
+			Line:    p.tokenizer.input,
+		}
 	}
-	lMTerm := p.parseMTerm()
+	lMTerm, err := p.parseMTerm()
+	if err != nil {
+		return nil, err
+	}
 
 	for !p.check(EOF) && !p.check(OR) && !p.check(RPAREN) {
 		// this must be correct
-		rMTerm := p.parseMTerm()
+		rMTerm, err := p.parseMTerm()
+		if err != nil {
+			return nil, err
+		}
 		lMTerm = &Concatenation{
 			Left:  lMTerm,
 			Right: rMTerm,
 		}
 	}
-	return lMTerm
+	return lMTerm, nil
 }
 
-func (p *Parser) parseExpr() ASTNode {
+func (p *Parser) parseExpr() (ASTNode, error) {
 	// handle empty expression, when the next token is EOF or ')' (it is wrapped in a pair of parentheses)
 	if p.check(EOF) || p.check(RPAREN) {
-		return nil
+		return nil, nil
 	}
-	lTerm := p.parseTerm()
-	for p.check(OR) {
-		_, err := p.match(OR)
-		if err != nil {
-			panic(err)
+	lTerm, err := p.parseTerm()
+	if err != nil {
+		return nil, err
+	}
+	if lTerm == nil {
+		return nil, &SyntaxError{
+			Column:  p.tokenizer.buf.Column,
+			Message: "expected a term",
+			Line:    p.tokenizer.input,
 		}
-		rTerm := p.parseTerm()
+	}
+	for p.check(OR) {
+		token, err := p.match(OR)
+		if err != nil {
+			return nil, &SyntaxError{
+				Column:  token.Column,
+				Message: "expected a | or EOF",
+				Line:    p.tokenizer.input,
+			}
+		}
+		rTerm, err := p.parseTerm()
+		if err != nil {
+			return nil, &SyntaxError{
+				Column:  p.tokenizer.buf.Column,
+				Message: "expected a term after |",
+				Line:    p.tokenizer.input,
+			}
+		}
 		if rTerm == nil {
-			panic("expected a term after |")
+			return nil, &SyntaxError{
+				Column:  p.tokenizer.buf.Column,
+				Message: "expected a term after |",
+				Line:    p.tokenizer.input,
+			}
 		}
 		lTerm = &Alternation{
 			Left:  lTerm,
 			Right: rTerm,
 		}
 	}
-	return lTerm
+	return lTerm, nil
 }
 
-func (p *Parser) parseGroup() ASTNode {
-	_, err := p.match(LPAREN)
+func (p *Parser) parseGroup() (ASTNode, error) {
+	token, err := p.match(LPAREN)
 	if err != nil {
-		panic(err)
+		return nil, &SyntaxError{
+			Column:  token.Column,
+			Message: "expected a (",
+			Line:    p.tokenizer.input,
+		}
 	}
-	expr := p.parseExpr()
-	_, err = p.match(RPAREN)
+	expr, err := p.parseExpr()
 	if err != nil {
-		panic(err)
+		return nil, err
+	}
+	token, err = p.match(RPAREN)
+	if err != nil {
+		return nil, &SyntaxError{
+			Column:  token.Column,
+			Message: "expected a )",
+			Line:    p.tokenizer.input,
+		}
 	}
 	return &Group{
 		Expr: expr,
-	}
+	}, nil
 }
 
-func (p *Parser) parseItem() ASTNode {
+func (p *Parser) parseItem() (ASTNode, error) {
 	if p.check(CHAR) {
 		return p.parseChar()
 	} else if p.check(LPAREN) {
 		return p.parseGroup()
 	} else {
-		panic("expected a Character or a Group")
+		return nil, &SyntaxError{
+			Column:  p.tokenizer.buf.Column,
+			Message: "expected a Character or a Group",
+			Line:    p.tokenizer.input,
+		}
 	}
 }
 
-func (p *Parser) parseMTerm() ASTNode {
-	item := p.parseItem()
+func (p *Parser) parseMTerm() (ASTNode, error) {
+	item, err := p.parseItem()
+	if err != nil {
+		return nil, err
+	}
 	var t ModifierType
-	var err error
 	if p.check(STAR) {
 		_, err = p.match(STAR)
 		t = ModifierStar
@@ -136,25 +210,33 @@ func (p *Parser) parseMTerm() ASTNode {
 		t = ModifierQuestion
 	} else if p.check(CHAR) || p.check(LPAREN) || p.check(RPAREN) || p.check(OR) || p.check(EOF) {
 		// if this is not a modified term
-		return item
+		return item, nil
 	} else {
-		panic("expected a '*', '+' or '?'")
+		return nil, &SyntaxError{
+			Column:  p.tokenizer.buf.Column,
+			Message: "expected a '*', '+' or '?'",
+			Line:    p.tokenizer.input,
+		}
 	}
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	return &Modifier{
 		Child: item,
 		Type:  t,
-	}
+	}, nil
 }
 
-func (p *Parser) parseChar() ASTNode {
+func (p *Parser) parseChar() (ASTNode, error) {
 	token, err := p.match(CHAR)
 	if err != nil {
-		panic(err)
+		return nil, &SyntaxError{
+			Column:  token.Column,
+			Message: "expected a character",
+			Line:    p.tokenizer.input,
+		}
 	}
 	return &Character{
 		Value: token.Value,
-	}
+	}, nil
 }
